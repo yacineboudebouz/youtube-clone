2865617a873e5d9de66128136e2623ff1ba2d170
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-10-09 17:50:35 +0200

    video

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import { CheckCircle } from '@mui/icons-material'
+import { Box, Stack, Typography } from '@mui/material'
+
+import { fetchFromApi } from '../utils/fetchFromApi'
+
 const VideoDetail = () => {
+    const { id } = useParams();
+    const [VideoDetail, setVideoDetail] = useState(null)
+    useEffect(() => {
+        fetchFromApi(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data.items[0]))
+    }, [id])
     return (
-        <div>VideoDetail</div>
+        <Box minHeight={'95vh'}>
+            <Stack direction={{ sx: 'column', md: 'row' }}>
+                <Box flex={1}>
+                    <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+                        <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+                        <Typography color='#fff' variant='hs' fontWeight='bold' p={2}>
+                            {/* {VideoDetail.snippet.title == null ? VideoDetail.snippet.title : ''} */}
+                        </Typography>
+                    </Box>
+                </Box>
+            </Stack>
+        </Box>
     )
 }
 
